92fb67904b739420e420dbca0540119ab982bcc6
TecBot <technic.robot.35@gmail.com>
2024-07-29 22:45:19 -0600

    Add extra internal id

--- scripts/migrate_schema.sql
+++ scripts/migrate_schema.sql
@@ -1,8 +1,5 @@
 -- To be run from root of repo
 -- sqlite3 db/twokinds_patreon.db  < scripts/create_schema.sql
-drop table patreon;
-drop table tags;
-
 CREATE TABLE IF NOT EXISTS "patreon_tmp"(
     "vault_id" INTEGER primary key,
     "id" INTEGER,
@@ -26,10 +23,10 @@ INSERT INTO patreon_tmp
 
 INSERT INTO tags_tmp
     (id, tag) 
-    SELECT id, tag from tags
+    SELECT id, tag from tags;
 
-ALTER TABLE patreon to patreon_bkp;
-ALTER TABLE patreon_tmp to patreon;
+ALTER TABLE patreon RENAME to patreon_bkp;
+ALTER TABLE patreon_tmp RENAME to patreon;
 
-ALTER TABLE tags to tags_bkp;
-ALTER TABLE tags_tmp to tags;
+ALTER TABLE tags RENAME to tags_bkp;
+ALTER TABLE tags_tmp RENAME to tags;
